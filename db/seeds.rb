d3326b24d46f9e53cc86b08d64d399cdb766941f
LordGoblin <maxime78600@gmail.com>
2019-01-31 02:13:22 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+5.times do
+  user = User.create!(name: Faker::HarryPotter.character)
+end
+
+require 'faker'
+5.times do
+  lien = Lien.create!(user_id: rand(1..5), http: Faker::HarryPotter.spell)
+end
+
+require 'faker'
+10.times do
+  com = Commentaire.create!(user_id: rand(1..5), lien_id: rand(1..5), content: Faker::HarryPotter.quote)
+end
+
+require 'faker'
 20.times do
   com = Comdecom.create!(user_id: rand(1..5), commentaire_id: rand(1..10), content: Faker::HarryPotter.house)
 end
